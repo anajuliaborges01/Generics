4eacf84afe664157cdd402d58c6be63cbcd5c6e8
Ana Julia <borgesanajulia55@gmail.com>
2023-01-23 10:35:57 +0000

    Set

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,35 +1,25 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
+import entities.Product;
+
 
 public class Program {
-
+	
 	public static void main(String[] args) {
 		
-		List<Shape> myShapes = new ArrayList<>();
-		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
-		
-		List<Circle> myCircles = new ArrayList<>();
-		myCircles.add(new Circle(2.0));
-		myCircles.add(new Circle(3.0));
-		
-		System.out.println("Total area: " + totalArea(myCircles));
-	}
-	
-	public static double totalArea(List<? extends Shape> list) {
-		double sum = 0.0;
-		for (Shape s : list) {
-			sum += s.area();
+		Set<Product> set = new HashSet<>();
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+		Product prod = new Product("Notebook", 1200.0);
+		System.out.println(set.contains(prod));
 		}
-		return sum;
-	}
-}
-	
+		}
+
+
+
 		
 		
--- src/entities/Products.java
+++ src/entities/Products.java
@@ -2,46 +2,30 @@ package entities;
 
 import java.util.Objects;
 
-public  class Product implements Comparable<Product>{
+public class Products {
 	private String name;
 	private Double price;
 	
-	public Product(String name, Double price) {
-		this.name = name;
-		this.price = price;
+	public Products(String name, Double price) {
+	this.name = name;
+	this.price = price;
 	}
-
 	public String getName() {
-		return name;
+	return name;
 	}
-
 	public void setName(String name) {
-		this.name = name;
+	this.name = name;
 	}
-
 	public Double getPrice() {
-		return price;
+	return price;
 	}
-
 	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	@Override
-	public String toString() {
-		return name + ", " + String.format("%.2f", price);
-	}
-
-	@Override
-	public int compareTo(Product other) {
-		return price.compareTo(other.getPrice());
-	}
-
+	this.price = price;
+}
 	@Override
 	public int hashCode() {
 		return Objects.hash(name, price);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -50,12 +34,7 @@ public  class Product implements Comparable<Product>{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Product other = (Product) obj;
+		Products other = (Products) obj;
 		return Objects.equals(name, other.name) && Objects.equals(price, other.price);
 	}
-
-	
-	}
-	
-	
-
+}
