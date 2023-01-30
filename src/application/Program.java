00735645b520e0cebbc540678b6d762d8401a2a1
Ana Julia <borgesanajulia55@gmail.com>
2023-01-30 13:23:03 +0000

    lambda1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,56 +1,32 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-import entities.LogEntry;
+import entities.Product;
 
 public class Program {
 	 public static void main (String []args) {
 		 
 		 
-		 
-		Scanner sc = new Scanner (System.in);
-		
-		System.out.print("Enter file full path : ");
-		String path = sc.nextLine();
-		
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			
-			Set <LogEntry>set = new HashSet<>();
-			
-			String line = br.readLine();
-			while(line!=null) {
-				
-				String [] fields =line.split(" ");
-				String username= fields [0];
-				Date moment = Date.from(Instant.parse(fields[1]));
-				
-				set.add(new LogEntry(username,moment));
-				line= br.readLine();
-			}	
-			
-			System.out.println("Total users: "+set.size() );
-			
-		}catch (IOException e ) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		
-		
-		sc.close();
+	List<Product>list = new ArrayList<>();
+	
+	list.add(new Product ("TV",900.00));
+    list.add(new Product("Notebook" , 1200.00));
+    list.add(new Product ("Tablet" , 450.00));
+    
+    list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+     
+	 
+     for(Product p:list) {
+    		System.out.println(p);
+    }
+	}
+	
 	 }
-		
-		
 
-		}
-		
 
 
 
